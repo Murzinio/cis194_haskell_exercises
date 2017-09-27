cc52057dfb1dbb62f15cc998eb292a7b674b1ddd
Murzinio <Murzinio@users.noreply.github.com>
2017-09-27 17:46:43 +0200

    Update and rename 1-4.hs to CIS_194.hs

--- CIS_194.hs
+++ CIS_194.hs
@@ -1,4 +1,4 @@
--- validate card number, exercises 1-4
+-- validate card number, exercises 1.1-1.4
 
 import Data.Digits
 
@@ -31,3 +31,15 @@ validate x
         10 
         == 0    = True
     | otherwise = False
+    
+
+-- print list of optimal moves to solve tower of hanoi, exercise 1.5
+
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+-- ps - peg source
+-- pt - peg target
+-- pm - peg middle
+hanoi 1 ps pt _ = [(ps, pt)]
+hanoi n ps pt pm = hanoi (n-1) ps pm pt  ++ [(ps, pt)] ++ hanoi (n-1) pm pt ps
