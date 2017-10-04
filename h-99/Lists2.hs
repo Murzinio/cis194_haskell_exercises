5be8edeb8dd33c6cc7933963e86f9109848ae6af
Murzinio <k.pawelczykbsh@gmail.com>
2017-10-04 18:01:03 +0200

    Exercises 13-16.

--- h-99/Lists2.hs
+++ h-99/Lists2.hs
@@ -29,7 +29,68 @@ encodeModified (x:xs) =
 -- Construct its uncompressed version.
 
 decodeModified :: [(EncodedElem a)] -> [a]
+decodeModified [] = []
 decodeModified [(Single x)] = [x]
 decodeModified [(Multiple c x)] = (replicate c x)
 decodeModified ((Single x):xs) = [x] ++ decodeModified xs
 decodeModified ((Multiple c x):xs) = (replicate c x) ++ decodeModified xs
+
+-- Problem 13
+-- Run-length encoding of a list (direct solution).
+
+-- Implement the so-called run-length encoding data compression method directly. 
+-- I.e. don't explicitly create the sublists containing the duplicates, as in problem 9, 
+-- but only count them. As in problem P11, 
+-- simplify the result list by replacing the singleton lists (1 X) by X.
+
+-- Seems the same as problem solved before.
+
+
+
+-- Problem 14
+-- Duplicate the elements of a list.
+
+dupli :: [a] -> [a]
+dupli [] = []
+dupli [x] = [x, x]
+dupli (x:xs) = [x, x] ++ dupli xs
+
+
+
+-- Problem 15
+-- Replicate the elements of a list a given number of times.
+
+repli :: [a] -> Int -> [a]
+repli [] _ = []
+repli _ 0 = []
+repli x 1 = x
+repli [x] n = [x] ++ repli [x] (n - 1)
+repli (x:xs) n = [x] ++ repli [x] (n - 1) ++ repli xs n
+
+
+
+-- Problem 16
+-- Drop every N'th element from a list.
+
+getHeadNTimes :: [a] -> Int -> [a]
+getHeadNTimes _ 0 = []
+getHeadNTimes [x] 1 = [x]
+getHeadNTimes x 1 = [head x]
+getHeadNTimes (x:xs) n = x : getHeadNTimes xs (n - 1)
+
+getListWithoutNHeads :: [a] -> Int -> [a]
+getListWithoutNHeads [] _ = []
+getListWithoutNHeads [_] 1 = []
+getListWithoutNHeads x 0 = x
+getListWithoutNHeads (_:xs) n = getListWithoutNHeads xs (n - 1)
+
+dropEvery :: [a] -> Int -> [a]
+dropEvery [] _ = []
+dropEvery _ 1 = []
+-- dropEvery (x:_:xs) 2 = x : dropEvery xs 2
+-- dropEvery (x:y:_:xs) 3 = x : y : dropEvery xs 3
+-- simple cases with n = 2 and n = 3 show emerging pattern that can be used for all n > 1
+dropEvery (x:xs) n
+    | 1 + (getLength xs) < n = x : xs
+    | otherwise              = [x] ++ (getHeadNTimes xs (n - 2)) 
+                                   ++ dropEvery (getListWithoutNHeads xs (n - 1)) n
