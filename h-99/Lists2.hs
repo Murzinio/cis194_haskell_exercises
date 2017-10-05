82fa94b644704e8b9ef1f8e3b31b4aab9f0897e4
Murzinio <k.pawelczykbsh@gmail.com>
2017-10-05 14:15:59 +0200

    Exercises 17-18

--- h-99/Lists2.hs
+++ h-99/Lists2.hs
@@ -73,20 +73,20 @@ repli (x:xs) n = [x] ++ repli [x] (n - 1) ++ repli xs n
 -- Drop every N'th element from a list.
 
 getHeadNTimes :: [a] -> Int -> [a]
-getHeadNTimes _ 0 = []
+getHeadNTimes _ 0   = []
 getHeadNTimes [x] 1 = [x]
-getHeadNTimes x 1 = [head x]
+getHeadNTimes xs 1   = [head xs]
 getHeadNTimes (x:xs) n = x : getHeadNTimes xs (n - 1)
 
 getListWithoutNHeads :: [a] -> Int -> [a]
 getListWithoutNHeads [] _ = []
 getListWithoutNHeads [_] 1 = []
-getListWithoutNHeads x 0 = x
+getListWithoutNHeads xs 0 = xs
 getListWithoutNHeads (_:xs) n = getListWithoutNHeads xs (n - 1)
 
 dropEvery :: [a] -> Int -> [a]
 dropEvery [] _ = []
-dropEvery _ 1 = []
+dropEvery _ 1  = []
 -- dropEvery (x:_:xs) 2 = x : dropEvery xs 2
 -- dropEvery (x:y:_:xs) 3 = x : y : dropEvery xs 3
 -- simple cases with n = 2 and n = 3 show emerging pattern that can be used for all n > 1
@@ -94,3 +94,38 @@ dropEvery (x:xs) n
     | 1 + (getLength xs) < n = x : xs
     | otherwise              = [x] ++ (getHeadNTimes xs (n - 2)) 
                                    ++ dropEvery (getListWithoutNHeads xs (n - 1)) n
+
+
+-- Problem 17
+-- Split a list into two parts; the length of the first part is given.
+
+-- Do not use any predefined predicates.
+
+split :: [a] -> Int -> ([a], [a])
+split [] _ = ([], [])
+split xs 0 = ([], xs)
+split xs n 
+    | n > getLength xs = error "List too short"
+    | otherwise        = (getHeadNTimes xs n, getListWithoutNHeads xs n)
+
+-- Problem 18
+-- Extract a slice from a list.
+
+-- Given two indices, i and k, 
+-- the slice is the list containing the elements between the i'th and k'th element of the original list (both limits included). 
+-- Start counting the elements with 1.
+
+getListWithoutNTails :: [a] -> Int -> [a]
+getListWithoutNTails [] _        = []
+getListWithoutNTails [_] 1       = []
+getListWithoutNTails xs 0        = xs
+-- getListWithoutNTails xs 1        = init xs
+-- getListWithoutNTails xs 2        = init (init xs)
+getListWithoutNTails (xs) n
+    | getLength xs < n = error "List too short"
+    | otherwise        = getListWithoutNTails (init xs) (n - 1)
+
+slice :: [a] -> Int -> Int -> [a]
+slice [] _ _ = []
+slice _  0 0 = [] 
+slice xs x y = getListWithoutNTails (getListWithoutNHeads xs (x - 1)) (getLength xs - y)
